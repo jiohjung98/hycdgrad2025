132f7ae5ede8bbe8d2642fcd3af729bbc163043d
정지오 <jiohjung@jeongjioui-MacBookAir.local>
2025-10-07 14:10:06 +0900

    feat: 파비콘

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,11 +33,6 @@ const nunitoSans = Nunito_Sans({
 export const metadata: Metadata = {
   title: "hycdgrad2025",
   description: "2025년도 한양대학교 ERICA 디자인대학 커뮤니케이션디자인학과 졸업전시회",
-  icons: {
-    icon: "/favi.svg",
-    shortcut: "/favi.svg",
-    apple: "/favi.svg",
-  },
 };
 
 export default function RootLayout({
@@ -47,6 +42,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
+      <head>
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="apple-touch-icon" href="/favicon.svg" />
+        <meta name="theme-color" content="#164e63" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${pinyonScript.variable} ${nunitoSans.variable} antialiased overflow-x-hidden`}
       >
